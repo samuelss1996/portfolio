296b5bf1a9b3d419d5e340f4fa8b87489cba2329
Samuel <samuelsoutullosobral@gmail.com>
2020-10-26 17:43:44 +0100

    Stylized footer (and whole home page)

--- src/app/component/section/social-networks/social-networks.component.ts
+++ src/app/component/section/social-networks/social-networks.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-social-networks',
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./social-networks.component.css']
 })
 export class SocialNetworksComponent implements OnInit {
+    @Input() public renderMode = 'black-text';
 
     constructor() {
     }
