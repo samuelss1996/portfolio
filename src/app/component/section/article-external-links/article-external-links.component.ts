78e66909877f79f4493be93f35c7c56a15f5cf8f
Samuel <samuelsoutullosobral@gmail.com>
2020-10-28 16:40:16 +0100

    Stylized article view

--- src/app/component/section/article-external-links/article-external-links.component.ts
+++ src/app/component/section/article-external-links/article-external-links.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-article-external-links',
-  templateUrl: './article-external-links.component.html',
-  styleUrls: ['./article-external-links.component.css']
+    selector: 'app-article-external-links',
+    templateUrl: './article-external-links.component.html',
+    styleUrls: ['./article-external-links.component.css']
 })
 export class ArticleExternalLinksComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
