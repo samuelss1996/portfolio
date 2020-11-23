81aa926187d0a9df84fc8fab8327ba3770d8fb2f
Samuel <samuelsoutullosobral@gmail.com>
2020-11-23 16:55:38 +0100

    Added 404 handler for API

--- src/server/controller/utils.js
+++ src/server/controller/utils.js
@@ -4,10 +4,14 @@ const Utils = {
 
     sendJson: function(response, err, data) {
         if(err || data === null) {
-            return response.status(500).send({status: 1, message: "Error while querying data", error: err});
+            return response.status(500).send({status: 500, message: "Error while querying data", error: err});
         }
 
-        return response.send({status: 0, data: data});
+        return response.send({status: 200, data: data});
+    },
+
+    sendNotFound: function(req, res) {
+        return res.status(404).send({status: 404, message: "API method not found"});
     }
 };
 
--- src/server/route/api.js
+++ src/server/route/api.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 const profileRouter = require("./profile");
 const articleRouter = require("./article");
+const Utils = require("../controller/utils");
 
 router.use("/profile", profileRouter);
 router.use("/article", articleRouter);
+router.all("/*", Utils.sendNotFound);
 
 module.exports = router;
