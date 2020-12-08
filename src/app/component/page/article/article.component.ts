d910789ee02f32bfe83b083fe4070864cc4d44f1
Samuel <samuelsoutullosobral@gmail.com>
2020-12-08 20:43:27 +0100

    Updated some images to decent resolution

--- src/app/component/page/article/article.component.ts
+++ src/app/component/page/article/article.component.ts
@@ -3,13 +3,13 @@ import {ActivatedRoute} from '@angular/router';
 import {LanguageService} from '../../../service/language.service';
 import {DomSanitizer} from '@angular/platform-browser';
 
-// TODO replace every itch.io picture with a good quality one
 // TODO add black semi-transparent overlay to "read more"
-// TODO add wanna see more projects? go to github in projects page
 // TODO replace every videogame with video game
 // TODO maybe add border to article list thumbnail
 // TODO put links to the other dudes
 // TODO add links to companies in experience?
+// TODO add wanna see more projects? go to github in projects page
+// TODO implement dynamic img resolution to reduce loading (?)
 // TODO translate
 // TODO update and paste CV as pdf
 // TODO fill portfolio article
