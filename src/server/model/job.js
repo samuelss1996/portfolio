b94cb5dabba85d7c8e7985fdf3565ba905060c66
Samuel <samuelsoutullosobral@gmail.com>
2020-12-10 20:02:42 +0100

    Translated home page

--- src/server/model/job.js
+++ src/server/model/job.js
@@ -4,14 +4,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const JobSchema = Schema({
-    position: String,
-    company: String,
+    position: {type: String, intl: true},
+    company: {type: String, intl: true},
     web: String,
-    location: String,
+    location: {type: String, intl: true},
     date_start: Date,
     date_end: Date,
     icon: String,
-    description: String
+    description: {type: String, intl: true}
+}, {
+    toJSON: {
+        virtuals: true
+    }
 });
 
 module.exports = mongoose.model("Job", JobSchema);
--- src/server/model/profile.js
+++ src/server/model/profile.js
@@ -3,11 +3,34 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const InterestSchema = Schema({
+    id: false,
+    icon: String,
+    text: {type: String, intl: true}
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+const ParagraphSchema = Schema({
+    id: false,
+    paragraph: {type: String, intl: true}
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
 const ProfileSchema = Schema({
     name: String,
     surname: String,
-    description: [String],
-    interests: [{icon: String, text: String}],
+    description: [ParagraphSchema],
+    interests: [InterestSchema],
+}, {
+    toJSON: {
+        virtuals: true
+    }
 });
 
 module.exports = mongoose.model("Profile", ProfileSchema);
