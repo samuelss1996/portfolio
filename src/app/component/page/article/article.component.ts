bbc59e93cfcae99761db27097c03fea01f1ca8b8
Samuel <samuelsoutullosobral@gmail.com>
2020-12-13 00:10:19 +0100

    Fixed some typos

--- src/app/component/page/article/article.component.ts
+++ src/app/component/page/article/article.component.ts
@@ -3,7 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {LanguageService} from '../../../service/language.service';
 import {DomSanitizer} from '@angular/platform-browser';
 
-// TODO check typos (telegram)
+// TODO change to not online yet on documents
 // TODO update readme.md
 // TODO update CV (add Spanish and remove phone number)
 // TODO add &origin=https://yourdomain.com to youtube iframes
